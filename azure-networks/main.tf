3c02f40debff2827f8f9a5b87b5c261b9e858d1f
PUSRAVA <zenith.sravan_kumar@mercedes-benz.com>
2025-05-19 18:52:50 +0800

    updated spacelift code

---
+++ azure-networks/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = "1.5.7"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = [var.vnet_address_space]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.subnet_address_prefix]
+}
---
+++ azure-networks/terraform.tfvars
@@ -0,0 +1,6 @@
+resource_group_name   = "example-rg"
+location              = "East US"
+vnet_name             = "example-vnet"
+vnet_address_space    = "10.0.0.0/16"
+subnet_name           = "example-subnet"
+subnet_address_prefix = "10.0.1.0/24"
