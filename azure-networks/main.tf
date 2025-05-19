6f0a5271d007f32154c17eae909afc9ea466fe54
PUSRAVA <zenith.sravan_kumar@mercedes-benz.com>
2025-05-19 18:55:58 +0800

    commented provider

--- azure-networks/main.tf
+++ azure-networks/main.tf
@@ -8,9 +8,9 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-}
+# provider "azurerm" {
+#   features {}
+# }
 
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
